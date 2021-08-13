dde7fb5c6204d6d16190728f19e40ad37e36d8b8
marwa <marwajawad19@gmail.com>
2021-08-13 11:27:12 -0400

    Building reducer

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends:[...state.friends,action.payload]
+      }
+      
+      case "friends/remove":
+        return {
+          friends:state.friends.filter(item=>item.id!=action.payload)
+        }
+  
+    default:
+    return state;
+  }
+ 
+  
 }
